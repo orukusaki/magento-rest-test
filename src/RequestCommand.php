ee1b6e48c2a7ced70e1ab72c2142e38886acf773
Peter Smith <psmith@sensiolabs.co.uk>
2016-01-22 10:56:55 +0000

    Slight tidy up

--- src/RequestCommand.php
+++ src/RequestCommand.php
@@ -5,6 +5,7 @@ namespace Orukusaki\MagentoRestTest;
 use Exception;
 use Cilex\Command\Command;
 use GuzzleHttp\Client;
+use GuzzleHttp\Message\RequestInterface;
 use GuzzleHttp\Stream\Stream;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Message\ResponseInterface;
@@ -49,19 +50,18 @@ class RequestCommand extends Command
         $client = $clientFactory($platformId);
 
         try {
-            $request = $client->createRequest($input->getOption('http_verb'), 'api/rest/' . ltrim($input->getArgument('http_resource'), '/'));
+            $request = $client->createRequest(
+                $input->getOption('http_verb'),
+                'api/rest/' .
+                ltrim($input->getArgument('http_resource'), '/')
+            );
+
             $errOutput->writeln('<info>Sending: ' . $request->getUrl() . '</info>');
 
             if ($input->getOption('request_content')) {
 
-                if (!is_string($input->getOption('request_type'))) {
-                    throw new Exception('request_type (-t) is a required parameter when request_content (-c) is used');
-                }
-
-                $body = Stream::factory($input->getOption('request_content'));
-                $request->setBody($body);
-                $request->setHeader('Content-Type', $this->parseContentType($input->getOption('request_type')));
-                $output->writeln('Request body: ' . $body, OutputInterface::OUTPUT_RAW);
+                $this->addRequestContent($input, $request);
+                $errOutput->writeln('Request body: ' . $request->getBody(), OutputInterface::OUTPUT_RAW);
             }
 
             /** @var ResponseInterface $response */
@@ -102,18 +102,17 @@ class RequestCommand extends Command
      * @param $type
      * @return string
      */
-    protected function parseContentType($type)
-    {
-        $types = array(
+    protected function parseContentType($type) {
+        $types = [
             'json' => 'application/json',
             'xml' => 'application/xml'
-        );
+        ];
 
         return (in_array($type, array_keys($types))) ? $types[$type] : (string) $type;
     }
 
     /**
-     * Format the response based on which http verb you are using
+     * Format the response
      * When you create an object through the rest API, Magento doesn't return a valid response and instead sets a
      * "location" header
      *
@@ -127,6 +126,26 @@ class RequestCommand extends Command
         }
 
         return json_encode($response->json(), JSON_PRETTY_PRINT);
+    }
+
+    /**
+     * @param InputInterface   $input
+     * @param RequestInterface $request
+     *
+     * @return Stream
+     * @throws Exception
+     */
+    protected function addRequestContent(InputInterface $input, RequestInterface $request)
+    {
+        $type = $input->getOption('request_type');
+
+        if (!is_string($type)) {
+            throw new Exception('request_type (-t) is a required parameter when request_content (-c) is used');
+        }
+
+        $body = Stream::factory($input->getOption('request_content'));
+        $request->setBody($body);
+        $request->setHeader('Content-Type', $this->parseContentType($type));
     }
 
     /**
